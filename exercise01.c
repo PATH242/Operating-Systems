de7dcc73432cb0139e0c5727154540ce8364917b
PATH242 <52926455+PATH242@users.noreply.github.com>
2021-09-21 21:59:27 +0300

    Add files via upload

---
+++ exercise01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void* threadOut( void* a){
+	printf("Welcome to this thread\tit has ID of: %lu\n", *(unsigned long*)a);
+	pthread_exit(NULL);
+}
+int main(){
+	int n=3;
+	for(int i=0;i<n;i++){
+		pthread_t threadID;
+		pthread_create(&threadID, NULL, threadOut,&threadID);
+		printf("creation of thread is complete: %lu\n", threadID);
+	}
+	return 0;
+}
