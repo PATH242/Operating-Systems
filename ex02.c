5747d0042028911c12055dad5ca9c885c18c6a1e
Mennatullah Awadallah <52926455+PATH242@users.noreply.github.com>
2021-11-09 22:34:34 +0300

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <linux/input.h>
+#include <sys/time.h>
+#include <error.h>
+#include <stdbool.h>
+int main(){
+    
+    char* dev = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+    
+    int fd = open(dev,O_RDONLY);
+    if(fd < 0){
+        return EXIT_FAILURE;
+    }
+    else{
+        struct input_event evt;
+        while(1){
+            read(fd,&evt,sizeof(evt));
+            if(evt.value == 0 && evt.type == EV_KEY)
+                printf("RELEASED 0x%04x (%d)\n", evt.code, evt.code);
+            else 
+            if (evt.value == 1 && evt.type == EV_KEY)
+                printf("PRESSED 0x%04x (%d)\n", evt.code, evt.code);
+            else
+            if(evt.type == EV_KEY){
+                printf("value %d, code[%d,%x], type %d",evt.value,evt.code, evt.code, evt.type);
+            }
+            fflush(stdout);
+        } 
+    }
+    return EXIT_SUCCESS;
+}
